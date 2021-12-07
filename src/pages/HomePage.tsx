66645b4f49927bb7f3a3ac37b2248e50cb1aac6f
Niko Draca <me@nikodraca.com>
2021-12-07 09:44:23 -0500

    init

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,29 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+
+import { Match } from '../types';
 
 const StyledDiv = styled.div`
   display: flex;
 `;
 
-export const HomePage = () => <StyledDiv>Hello world</StyledDiv>;
+export const HomePage = () => {
+  const [matches, setMatches] = useState<Array<Match>>([]);
+
+  const handleFileUploadChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target && e.target.files) {
+      const fileReader = new FileReader();
+      fileReader.readAsText(e.target.files[0], 'UTF-8');
+      fileReader.onload = (e) => {
+        setMatches(JSON.parse(e.target?.result as string));
+      };
+    }
+  };
+
+  console.log(matches);
+  return (
+    <StyledDiv>
+      <input type="file" onChange={handleFileUploadChange} />
+    </StyledDiv>
+  );
+};
--- src/types.tsx
+++ src/types.tsx
@@ -1,3 +1,19 @@
-export interface ExampleInterface {
-  example: string;
+interface Metadata {
+  timestamp: string;
+  type: string;
+}
+
+interface BlockMetadata extends Metadata {
+  block_type: string;
+}
+
+interface ChatMetadata extends Metadata {
+  body: string;
+}
+
+export interface Match {
+  block?: Array<BlockMetadata>;
+  like?: Array<Metadata>;
+  match?: Array<Metadata>;
+  chats?: Array<ChatMetadata>;
 }
